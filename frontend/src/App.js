8aa072359420104d9865840e4a776c51487eb18d
Nelson M <mwinel@live.com>
2022-10-26 17:02:56 +0300

    Fix loading states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,10 +55,8 @@ function App() {
           <span>Loading...</span>
         ) : (
           <>
-            {images.length > 0 ? (
-              <ul
-                className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
-              >
+            {images.length ? (
+              <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {images.map((image) => (
                   <li key={image.links[0].href} className="relative w-44 h-36">
                     <ImageCard imageName={image.links[0].href} />
