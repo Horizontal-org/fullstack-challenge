0c452dc8d04ce670cc3b65e80f83a63d21cdb52b
Nelson M <mwinel@live.com>
2022-10-27 07:57:50 +0300

    Add save image API

--- backend/src/modules/file/controllers/upload.file.controller.ts
+++ backend/src/modules/file/controllers/upload.file.controller.ts
@@ -1,13 +1,16 @@
 import {
   Controller,
   Post,
-  Body,
+  Get,
+  Query,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { createWriteStream } from 'fs';
+import * as https from 'https';
 
 @Controller('file')
 export class UploadFileController {
@@ -34,5 +37,30 @@ export class UploadFileController {
       file: file,
     };
     return response;
+  }
+
+  @Get('save')
+  async getFile(@Query() fileURL: any) {
+    
+    const saveImage = (url, path) => {
+      const fullURL = url;
+      const localPath = createWriteStream(path);
+      const request = https.get(fullURL, function (response) {
+        response.pipe(localPath);
+      });
+    };
+
+    const { image } = fileURL;
+    const randomName = Array(32)
+      .fill(null)
+      .map(() => Math.round(Math.random() * 16).toString(16))
+      .join('');
+    const extensionName = extname(image);
+    saveImage(image, './data/' + randomName + '.' + extensionName);
+    const file = {
+      imageURL: this.SERVER_URL + randomName + '.' + extensionName,
+    };
+
+    return file;
   }
 }
